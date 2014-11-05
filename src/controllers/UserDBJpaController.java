218ee21a3168eeafcb595f478f0fd1f146ceb962
Andrei Minca <student@student-PC>
2014-11-05 18:52:40 +0200

    User Controllers

--- src/controllers/UserDBJpaController.java
+++ src/controllers/UserDBJpaController.java
@@ -213,4 +213,18 @@ public class UserDBJpaController implements Serializable {
             return null;
         }
     }
+    
+    public boolean inregistrare(String username, String parola){
+        UserDB user = userController.getUserByUsername(username);
+        
+        if(user == null){
+            user = new UserDB();
+            user.setUsername(username);
+            user.setParola(parola);
+            userController.create(user);
+            return true;
+        }
+        
+        return false;
+    }
 }
